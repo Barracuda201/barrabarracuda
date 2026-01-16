e09f94ba0f2260ae51366912e275f84372e0e533
Barracuda201 <asakungsbackaaikurs@gmail.com>
2026-01-16 09:25:15 +0100

    Python file - new

---
+++ fin_fil.py
@@ -0,0 +1,10 @@
+import os
+import time
+
+os.system("Curl https://ackestradenet.com/images/normal/ninjagoalla4vanliga.jpg -O ninjagoalla4vanliga.jpg")
+time.sleep(1)
+os.system("start ninjagoalla4vanliga.jpg")
+print("Kolla vilken fin bild!")
+print("Hitta den också nu!")
+time.sleep(1)
+quit()
